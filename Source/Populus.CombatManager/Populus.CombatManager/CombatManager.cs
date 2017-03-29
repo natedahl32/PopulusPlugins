622bfb3d85e28665531afba0415bfb6e376ab2dc
Nate Dahl <nndahl@gmail.com>
2017-03-28 20:24:53 -0500

    Added CombatManager plugin base

---
+++ Source/Populus.CombatManager/Populus.CombatManager/CombatManager.cs
@@ -0,0 +1,26 @@
+﻿using Populus.Core.Plugins;
+
+namespace Populus.CombatManager
+{
+    public class CombatManager : PluginBase
+    {
+        #region Properties
+
+        public override string Name => "Combat Manager";
+
+        public override string Author => "Kazadoom";
+
+        public override string Website => "";
+
+        #endregion
+
+        #region Public Methods
+
+        public override void Initialize()
+        {
+            
+        }
+
+        #endregion
+    }
+}
